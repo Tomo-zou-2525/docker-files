2d56aceaef459e1289b408ecda51f5f63f856756
友造-2525 <bathbook1990tomozou@gmail.com>
2020-07-27 21:09:08 +0900

    feature/リファクタリング

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
  class BoardsController < ApplicationController
+   before_action :set_target_board, only: %i[show edit update destroy]
+
    def index
      @boards = Board.all
    end
@@ -7,27 +9,40 @@
      @board = Board.new
    end 
    def create
-     Board.create(board_params)
+     board = Board.create(board_params)
+
+     redirect_to board #このboardには１０行目でidが付与されている、なのでredirect時にもidが効く
    end
 
    def show
-     @board = Board.find(params[:id])
+     
    end
 
    def edit
-     @board = Board.find(params[:id])
+     
    end
 
    def update
-     board = Board.find(params[:id])
+     
      board.update(board_params)
 
-     redirect_to board
+     redirect_to @board
+   end
+
+   def destroy
+   	 #特定のIDに対して行うアクションなので、findがいる
+   	 @board.delete
+
+   	 redirect_to boards_path
    end
 
    private
 
    def board_params
      params.require(:board).permit(:name, :title, :body)
+   end
+
+   def set_target_board
+   	 @board = Board.find(params[:id])
    end
  end
