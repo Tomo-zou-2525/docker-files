49b9f8c28fb8be2867bafb2c70eb774b27a757b7
友造-2525 <bathbook1990tomozou@gmail.com>
2020-07-28 19:02:14 +0900

    feature/投稿機能にコメントを追加

--- Gemfile
+++ Gemfile
@@ -31,10 +31,14 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'bootstrap', '~> 4.0.0'
 gem 'mini_racer'
+gem 'kaminari'
+gem 'rails-i18n'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rails-flog', require: 'flog'
 end
 
 group :development do
@@ -45,6 +49,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'pry-byebug'
+  gem 'annotate', '2.4.1.beta1'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,20 +2,32 @@
    before_action :set_target_board, only: %i[show edit update destroy]
 
    def index
-     @boards = Board.all
+     # @boards = Board.all
+     @boards = Board.page(params[:page]) #ページネーション追加のため
    end
 
    def new
-     @board = Board.new
+     @board = Board.new(flash[:board])
    end 
-   def create
-     board = Board.create(board_params)
 
-     redirect_to board #このboardには１０行目でidが付与されている、なのでredirect時にもidが効く
+   def create
+	 # board = Board.create(board_params)
+	 board = Board.new(board_params)
+	 if board.save
+	   flash[:notice] = "『#{board.title}』の掲示板を作成しました"
+	   redirect_to board
+	 else
+	   redirect_to new_board_path, flash: {
+	  	 board: board,
+	  	 error_messages: board.errors.full_messages
+	   }
+	 end  
+	# flash[:notice] = "『#{board.title}』の掲示板を作成しました"
+	# redirect_to board #このboardには１０行目でidが付与されている、なのでredirect時にもidが効く
    end
 
    def show
-     
+     @comment = @board.comments.new #board.comments(boardオブジェクトのcomments)にnewメソッドを呼び出すことで、掲示板に紐づいた@commentが取得できる
    end
 
    def edit
@@ -33,7 +45,7 @@
    	 #特定のIDに対して行うアクションなので、findがいる
    	 @board.delete
 
-   	 redirect_to boards_path
+   	 redirect_to boards_path, flash: { notice: "『#{@board.title}』の掲示板を削除しました" } 
    end
 
    private
